12c5491d4699765a538ac0ca0c759f804785fc8d
mrtnrs <github@gzt.be>
2023-04-14 19:58:48 +0200

    vcl

---
+++ pw-frontend/app/vercelFix/Icon/index.js
@@ -0,0 +1,8 @@
+// ** Icon Imports
+import { Icon } from '@iconify/react'
+
+const CustomIcon = ({ icon, ...rest }) => {
+  return <Icon icon={icon} fontSize='1.5rem' {...rest} />
+}
+
+export default CustomIcon
