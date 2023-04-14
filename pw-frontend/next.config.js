11aa5372d18c085c0ee3ece78ac2f2547cfe96f0
mrtnrs <github@gzt.be>
2023-04-14 17:53:18 +0200

    vcl

--- pw-frontend/next.config.js
+++ pw-frontend/next.config.js
@@ -1,18 +1,8 @@
-const path = require('path');
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, 'app'),
-    };
+}
 
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
