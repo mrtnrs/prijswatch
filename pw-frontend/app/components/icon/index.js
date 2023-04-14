5c55f86b4e1ce31fca58019dce3514a1d4da613e
mrtnrs <github@gzt.be>
2023-04-14 19:55:19 +0200

    linux casesensitive issue

--- pw-frontend/app/components/icon/index.js
+++
@@ -1,8 +0,0 @@
-// ** Icon Imports
-import { Icon } from '@iconify/react'
-
-const CustomIcon = ({ icon, ...rest }) => {
-  return <Icon icon={icon} fontSize='1.5rem' {...rest} />
-}
-
-export default CustomIcon
